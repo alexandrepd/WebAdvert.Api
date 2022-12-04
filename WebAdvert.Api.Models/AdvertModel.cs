14c85b3896a89adf3848e2ad78760f95552c67c2
Alexandre Oliveira <alexandre_pd@hotmail.com>
2022-12-04 13:27:01 +0000

    adding models

---
+++ WebAdvert.Api.Models/AdvertModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAdvert.Api.Models
+{
+    public class AdvertModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ WebAdvert.Api.Models/AdvertStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Api.Models
+{
+    public enum AdvertStatus
+    {
+        Pending = 1,
+        Active = 2
+
+    }
+}
